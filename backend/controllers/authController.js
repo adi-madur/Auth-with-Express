5c9b930caab15244fc5af2b1c2eefd330cbcc52f
adi-madur <adityamadur735@gmail.com>
2024-01-14 14:59:56 +0530

    signup

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,10 +1,59 @@
+const userModel = require("../models/userSchema");
+const emailValidator = require('email-validator');
+
+
 const signup = async (req, res, next) => {
     const { name, email, password, confirmPassword } = req.body;
 
-    return res.status(200).json({
-        success: true,
-        data : {}
-    })
+    // Checking if all fields are provided
+    if(!name || !email || !password || !confirmPassword){
+        return res.status(400).json({
+            success: false,
+            message: "Every field is required"
+        })
+    } 
+
+    // Validating Email
+    const validEmail = emailValidator.validate(email);
+    if(!validEmail){
+        return res.status(400).json({
+            success: false,
+            message: "Please Enter a valid Email"
+        })
+    }
+
+    if (password !== confirmPassword){
+        return res.status(400).json({
+            success: false,
+            message: "Password and Confirm Password doesn't match"
+        })
+    }
+
+
+    try {
+
+        const userInfo = userModel(req.body); // --> the Variable `userInfo` (is now a Schema) now has taken structure of Schema and the same key names from `req.body` are matched with Schema key names...
+        //  We can save data 
+        const result = await userInfo.save(); //--> or we could have used userModel.create({...}) /A Schema name/.create
+
+        return res.status(200).json({
+            success: true,
+            data: result
+        })
+    } catch (e) {
+
+        if (e.code === 11000) { //--> 11000 code is a error code for duplicate key. i.e same email
+            return res.status(400).json({
+                success: false,
+                message: "Account already exists with this email"
+            })
+        }
+
+        return res.status(400).json({
+            success: false,
+            message: e.message
+        })
+    }
 
 }
 
