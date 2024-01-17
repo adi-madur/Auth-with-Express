53ef564361980705d949b7d717186b2f58608b23
adi-madur <adityamadur735@gmail.com>
2024-01-17 12:26:25 +0530

    middleware setup

--- backend/app.js
+++ backend/app.js
@@ -2,10 +2,13 @@ const express = require('express');
 const app = express();
 const authRoute = require('./routes/authRoute.js');
 const connectToDatabase = require('./config/databaseConfig.js');
+const cookieParser = require('cookie-parser');
 
 connectToDatabase();
 
 app.use(express.json()); //--> To accept json as data
+app.use(cookieParser()); // --> It is used so that, the cookie will be parsed in json format for further processing
+
 app.use('/api/auth/', authRoute);
 
 
--- backend/routes/authRoute.js
+++ backend/routes/authRoute.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const authRouter = express.Router();
-const { signup, signin, getUser } = require('../controllers/authController.js')
+const { signup, signin, getUser } = require('../controllers/authController.js');
+const jwtAuth = require('../middleware/jwtAuth.js');
 
 authRouter.post('/signup', signup);
 authRouter.post('/signin', signin);
-authRouter.get('/user', getUser);
+authRouter.get('/user', jwtAuth, getUser);
 
 module.exports = authRouter;
