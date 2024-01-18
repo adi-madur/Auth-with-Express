2ead10c4a1eba15ebe496c05e83f20aeef0d6de4
adi-madur <adityamadur735@gmail.com>
2024-01-18 17:38:05 +0530

    CORS setup

--- backend/app.js
+++ backend/app.js
@@ -3,11 +3,19 @@ const app = express();
 const authRoute = require('./routes/authRoute.js');
 const connectToDatabase = require('./config/databaseConfig.js');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
 
 connectToDatabase();
 
 app.use(express.json()); //--> To accept json as data
 app.use(cookieParser()); // --> It is used so that, the cookie will be parsed in json format for further processing
+
+// CORS Configurations
+app.use(cors({
+    origin: [process.env.CLIENT_URL], // --> Allows access to the mentioned URL in .env file
+    credentials: true // --> Allows Cookies to set by the website mentioned..
+
+}))
 
 app.use('/api/auth/', authRoute);
 
